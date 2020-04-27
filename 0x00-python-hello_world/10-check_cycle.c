b61a9d9e944db2bf09129a709efa69cbba011cf8
Cynthia Taylor <820@holbertonschool.com>
2020-04-27 20:32:35 +0000

    Change back to while loop

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -14,12 +14,13 @@ int check_cycle(listint_t *list)
 	slow = list;
 	fast = list;
 
-	do {
+	while (slow && fast && fast->next)
+	{
 	       	slow = slow->next;
 	       	fast = fast->next->next;
 
 		if (slow == fast)
 			return (1);
-	} while (slow && fast && fast->next);
+	}
 	return (0);
 }
