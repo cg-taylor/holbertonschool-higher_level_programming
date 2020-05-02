cf450b4c2df21cc8b7eb63c018b131f6c4d6822e
Cynthia Taylor <820@holbertonschool.com>
2020-05-02 19:32:05 +0000

    Fix logic errors in for loop

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -5,6 +5,6 @@ if __name__ == "__main__":
     argc = len(sys.argv) - 1
     sum = 0
 
-    for i in range(1, argc):
-        sum = sum + int(sys.argv[argc])
+    for i in range(1, argc + 1):
+        sum = sum + int(sys.argv[i])
     print(sum)
