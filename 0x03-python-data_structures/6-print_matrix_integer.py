988282fe570832f6a7d97892ec92dce820a91792
Cynthia Taylor <820@holbertonschool.com>
2020-05-06 16:50:49 +0000

    Fix empty matrix case

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -3,11 +3,9 @@
 
 def print_matrix_integer(matrix=[[]]):
     if not any(matrix):
-        return None
+        print()
 
     for row in matrix:
-        if len(row) == 0:
-            print()
         for idx in range(len(row)):
             print("{:d}".format(row[idx]),
                   end="\n" if idx is len(row) - 1 else " ")
