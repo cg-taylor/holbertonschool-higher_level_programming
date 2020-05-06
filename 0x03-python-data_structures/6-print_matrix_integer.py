bd6077b38ec5529a36836e39fcf44d651b48dbc6
Cynthia Taylor <820@holbertonschool.com>
2020-05-06 16:41:37 +0000

    Add working version of task 6

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -9,4 +9,4 @@ def print_matrix_integer(matrix=[[]]):
         if len(row) == 0:
             print()
         for idx in range(len(row)):
-            print("{:d}".format(row[idx], end="\n" if idx is len(row) - 1 else " "))
+            print("{:d}".format(row[idx]), end="\n" if idx is len(row) - 1 else " ")
