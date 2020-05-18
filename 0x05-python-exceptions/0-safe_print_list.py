10d07f8313be91180d040c5c0469d6cbc8a969d9
Cynthia Taylor <820@holbertonschool.com>
2020-05-18 20:19:38 +0000

    Remove integer specific printing

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -5,7 +5,7 @@ def safe_print_list(my_list=[], x=0):
     idx = 0
     try:
         while idx < x:
-            print("{:d}".format(my_list[idx]),
+            print("{}".format(my_list[idx]),
                   end="\n" if idx is x - 1 else "")
             idx = idx + 1
         return x
