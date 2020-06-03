b07936c05f1eef4368a60f283a65108f6192a2f9
Cynthia Taylor <820@holbertonschool.com>
2020-06-03 01:05:54 +0000

    Replace explicit definition of BaseGeometry with import statement

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -2,28 +2,7 @@
 """BaseGeometry module - no test cases needed"""
 
 
-class BaseGeometry:
-    """Define a base class called BaseGeometry"""
-
-    def area(self):
-        """Calculate area"""
-        raise Exception('area() is not implemented')
-
-    def integer_validator(self, name, value):
-        """Determines if value is an integer
-
-        Args:
-            name: the name of the variable, always a string
-            value: the value of the variable
-
-        Raises:
-            TypeError: if value is not an integer
-            ValueError: if value is <= 0
-        """
-        if type(value) != int:
-            raise TypeError('{} must be an integer'.format(name))
-        if value <= 0:
-            raise ValueError('{} must be greater than 0'.format(name))
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
