07b5db12ee1af8ea3c9b449c155c9dbeec13e4ce
Cynthia Taylor <820@holbertonschool.com>
2020-06-11 18:20:43 +0000

    Fix PEP8 style error

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-
-
 def square_matrix_simple(matrix=[]):
-    return list(map(lambda squared: list(map(lambda x: x**2, squared)), matrix))
+    return list(map(lambda squared: list(map(lambda x: x**2,
+                                             squared)), matrix))
